0be7db0da858d1ea186aa8ad63fde83f054af58b
Aravind Samala <52457571+thyaravind@users.noreply.github.com>
2021-04-23 07:18:28 -0400

    initializing

---
+++ Constants/sdgs.js
@@ -0,0 +1,18 @@
+export const sdgs = [{value:1,text:"SDG 1: No Poverty"},
+    {value:2,text:"SDG 2: Zero Hunger"},
+    {value:3,text:"SDG 3: Good Health and Well-Being"},
+    {value:4,text:"SDG 4: Quality Education"},
+    {value:5,text:"SDG 5: Gender Equality"},
+    {value:6,text:"SDG 6: Clean Water and Sanitation"},
+    {value:7,text:"SDG 7: Affordable and Clean Energy"},
+    {value:8,text:"SDG 8: Decent Work and Economic Growth"},
+    {value:9,text:"SDG 9: Industry, Innovation and Infrastructure"},
+    {value:10,text:"SDG 10: Reduced Inequalities"},
+    {value:11,text:"SDG 11: Sustainable Cities and Communities "},
+    {value:12,text:"SDG 12: Responsible Consumption and Production"},
+    {value:13,text:"SDG 13: Climate Action"},
+    {value:14,text:"SDG 14: Life Below Water"},
+    {value:15,text:"SDG 15: Life on Land"},
+    {value:16,text:"SDG 16: Peace, Justice and Strong Institutions"},
+    {value:17,text:"SDG 17: Partnerships for the Goals"}
+]
